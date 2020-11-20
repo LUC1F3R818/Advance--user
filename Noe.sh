9446b9660bea31d6b6409fe6b40d6f13c5ece286
LUC1F3R818 <74072072+LUC1F3R818@users.noreply.github.com>
2020-11-20 16:31:17 +0630

    Update Noe.sh

--- Noe.sh
+++ Noe.sh
@@ -7,30 +7,33 @@ read -p "Password"
 if [[ $Username == $Username || $Password == $Password ]];then
 echo ""
 clear
-echo "Welcome"
+figlet "Welcome"|lolcat
 else 
 echo ""
-echo "Incorrect" 
+echo "wrong password. Ma a loe "|lolcat
 fi
 figlet B4NDU14 |lolcat
 echo -e "\e[1;32m Myanmar script kiddies \e[0m"
 echo -e "\e[1;32m Sayar Yell Phone Naing \e[0m"
 echo -e "\e[1;32m Easily install the best tools"
-echo -e "\e[1;32m (1)Sqlmap \e[0m"
-echo -e "\e[1;32m (2)T-Header \e[0m"
-echo -e "\e[1;32m (3)DDos-Attack \e[0m"
-echo -e "\e[1;32m (4)Myanmar logo \e[0m"
-echo -e "\e[1;32m (5) Metasploit Framework\e[0m"
-echo -e "\e[1;32m (6)hasher \e[0m" 
-echo -e "\e[1;32m (7)useful-tool \e[0m"
-echo -e "\e[1;32m (8)tmvenom \e[0m" 
-echo -e "\e[1;32m (9)cyberscan \e[0m" 
-echo -e "\e[1;32m (10) Use for fun \e[0m" 
-echo -e "\e[1;32m (00)Exit \e[0m"
-echo -e ""
-read -p "Choose a number~#":$input
-if [$input=1] || [$input=01]
+echo -e "\e[1;32m 1.Sqlmap \e[0m"
+echo -e "\e[1;32m 2.T-Header \e[0m"
+echo -e "\e[1;32m 3.DDos-Attack \e[0m"
+echo -e "\e[1;32m 4.Myanmar logo \e[0m"
+echo -e "\e[1;32m 5.Metasploit Framework\e[0m"
+echo -e "\e[1;32m 6.hasher \e[0m" 
+echo -e "\e[1;32m 7.useful-tool \e[0m"
+echo -e "\e[1;32m 8.tmvenom \e[0m" 
+echo -e "\e[1;32m 9.cyberscan \e[0m" 
+echo -e "\e[1;32m 10.Use for fun \e[0m" 
+echo -e "\e[1;32m 00.Exit \e[0m"
+echo ""
+read -p "Choose a number~#!" act
+
+
+if [ $act = 1 ] || [ $act = 1 ]
 then
+clear
 apt update
 apt upgrade
 apt install git
@@ -41,18 +44,24 @@ git clone https://github.com/sqlmapproject/sqlmap
 cd sqlmap
 python2 sqlmap.py -h
 fi 
-figlet B4NDU14 |lolcat
-if [$input=2] || [$input=02]
+
+
+if [ $act = 2 ] || [ $act = 02 ]
 then
+clear
+figlet B4NDU14 |lolcat
 apt update
 apt upgrade
 git clone https://github.com/remo7777/T-Header
 cd T-Header
 bash T-Header.sh
 fi
-figlet Cyber bullet |lolcat
-if [$input=3] || [$input=03]
+
+
+if [ $act = 3 ] || [ $act = 03 ]
 then
+clear
+figlet Cyber bullet |lolcat
 apt update
 apt upgrade
 apt install git
@@ -60,10 +69,12 @@ git clone https://github.com/Techtushar07/DDos-Attack.git
 cd DDos-Attack
 python2 ddos-attack.py
 fi
+
+
+if [ $act = 4 ] || [ $act = 04 ]
+then
 clear
 figlet OXYG3N |lolcat
-if [$input=4] || [$input=04]
-then
 pkg update && pkg upgrade 
  pkg install bash
  pkg install python
@@ -74,30 +85,36 @@ pkg install git
 chmod +x *
 bash NayMyo.sh
 fi
-clear 
-figlet Myanmar B14CK 4TT4CK3R Team |lolcat
-if [$input=5] || [$input=05]
+
+
+if [ $act = 5 ] || [ $act = 05 ]
 then
+clear
+figlet Myanmar B14CK 4TT4CK3R Team |lolcat
 pkg install unstable-repo
 pkg install metasploit
 msfconsole
 fi
+
+
+if [ $act = 6 ] || [ $act = 06 ]
+then
 clear
 figlet Myanmar |lolcat
-if [$input=6] || [$input=06]
-then
 apt update
-$ apt upgrade
-$ apt install git
-$ apt install python
-$ apt install python2
-$ git clone https://github.com/ciku370/hasher
+apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/ciku370/hasher
 cd hasher
 fi
-clear
-figlet Cyber bullet |lolcat
-if [$input=7] || [$input=07]
+
+
+if [ $act = 7 ] || [ $act = 07 ]
 then
+clear
+figlet Error |lolcat
 apt update && apt upgrade -y
 apt install python
 apt install python2
@@ -106,10 +123,12 @@ git clone https://github.com/LUC1F3R818/useful-tool
 cd useful-tool
 bash Project.sh
 fi
-clear
-figlet B14CK 4TT4CK3R  |lolcat
-if [$input=8] || [$input=08]
+
+
+if [ $act = 8 ] || [ $act = 08 ]
 then
+clear
+figlet Ma a loe |lolcat
 apt update
 apt upgrade
 apt install git
@@ -117,10 +136,12 @@ apt install git
  git clone https://github.com/TechnicalMujeeb/tmvenom
  cd tmvenom
 fi
+
+
+if [ $act = 9 ] || [ $act = 09 ]
+then
 clear
 figlet B14CK H4T |lolcat
-if [$input=9] || [$input=09]
-then
 apt update && apt upgrade
 apt install git
 apt install python2
@@ -128,15 +149,21 @@ apt install python
  git clone https://github.com/medbenali/CyberScan.git
 cd CyberScan
 fi
+
+
+if [ $act = 10 ] || [ $act = 100 ]
+then
 clear
 figlet Cyber Bullet |lolcat
-if [$input=10] || [$input=10]
-then
 pkg install libcaca
 cacafire
 fi
-clear
+
+
 figlet Bye Bye |lolcat
-if [$input=0] || [$input=0]
+if [ $act = 0 ] || [ $act = 00 ]
 then
+clear
 figlet bye bye |lolcat
+fi
+
