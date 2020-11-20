b87b819b2009db598d41e9091e6434d23a568ce5
LUC1F3R818 <74072072+LUC1F3R818@users.noreply.github.com>
2020-11-20 08:03:31 +0630

    Add files via upload

---
+++ Noe.sh
@@ -0,0 +1,142 @@
+green='\033[37;1m'
+figlet Cyber bullet  |lolcat
+ set Username=Cyber bullet
+set Password=Cyber bullet
+read -p "Username"
+read -p "Password"
+if [[ $Username == $Username || $Password == $Password ]];then
+echo ""
+clear
+echo "Welcome"
+else 
+echo ""
+echo "Incorrect" 
+fi
+figlet B4NDU14 |lolcat
+echo -e "\e[1;32m Myanmar script kiddies \e[0m"
+echo -e "\e[1;32m Sayar Yell Phone Naing \e[0m"
+echo -e "\e[1;32m Easily install the best tools"
+echo -e "\e[1;32m (1)Sqlmap \e[0m"
+echo -e "\e[1;32m (2)T-Header \e[0m"
+echo -e "\e[1;32m (3)DDos-Attack \e[0m"
+echo -e "\e[1;32m (4)Myanmar logo \e[0m"
+echo -e "\e[1;32m (5) Metasploit Framework\e[0m"
+echo -e "\e[1;32m (6)hasher \e[0m" 
+echo -e "\e[1;32m (7)useful-tool \e[0m"
+echo -e "\e[1;32m (8)tmvenom \e[0m" 
+echo -e "\e[1;32m (9)cyberscan \e[0m" 
+echo -e "\e[1;32m (10) Use for fun \e[0m" 
+echo -e "\e[1;32m (00)Exit \e[0m"
+echo -e ""
+read -p "Choose a number~#":$input
+if [$input=1] || [$input=01]
+then
+apt update
+apt upgrade
+apt install git
+apt inatall python
+apt inatall python2
+apt install php
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py -h
+fi 
+figlet B4NDU14 |lolcat
+if [$input=2] || [$input=02]
+then
+apt update
+apt upgrade
+git clone https://github.com/remo7777/T-Header
+cd T-Header
+bash T-Header.sh
+fi
+figlet Cyber bullet |lolcat
+if [$input=3] || [$input=03]
+then
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Techtushar07/DDos-Attack.git
+cd DDos-Attack
+python2 ddos-attack.py
+fi
+clear
+figlet OXYG3N |lolcat
+if [$input=4] || [$input=04]
+then
+pkg update && pkg upgrade 
+ pkg install bash
+ pkg install python
+ pkg install python2
+pkg install git
+ git clone https://github.com/NayMyo969/MM-Logo.git
+ cd MM-Logo
+chmod +x *
+bash NayMyo.sh
+fi
+clear 
+figlet Myanmar B14CK 4TT4CK3R Team |lolcat
+if [$input=5] || [$input=05]
+then
+pkg install unstable-repo
+pkg install metasploit
+msfconsole
+fi
+clear
+figlet Myanmar |lolcat
+if [$input=6] || [$input=06]
+then
+apt update
+$ apt upgrade
+$ apt install git
+$ apt install python
+$ apt install python2
+$ git clone https://github.com/ciku370/hasher
+cd hasher
+fi
+clear
+figlet Cyber bullet |lolcat
+if [$input=7] || [$input=07]
+then
+apt update && apt upgrade -y
+apt install python
+apt install python2
+apt install git 
+git clone https://github.com/LUC1F3R818/useful-tool
+cd useful-tool
+bash Project.sh
+fi
+clear
+figlet B14CK 4TT4CK3R  |lolcat
+if [$input=8] || [$input=08]
+then
+apt update
+apt upgrade
+apt install git
+ apt install python2
+ git clone https://github.com/TechnicalMujeeb/tmvenom
+ cd tmvenom
+fi
+clear
+figlet B14CK H4T |lolcat
+if [$input=9] || [$input=09]
+then
+apt update && apt upgrade
+apt install git
+apt install python2
+apt install python
+ git clone https://github.com/medbenali/CyberScan.git
+cd CyberScan
+fi
+clear
+figlet Cyber Bullet |lolcat
+if [$input=10] || [$input=10]
+then
+pkg install libcaca
+cacafire
+fi
+clear
+figlet Bye Bye |lolcat
+if [$input=0] || [$input=0]
+then
+figlet bye bye |lolcat
